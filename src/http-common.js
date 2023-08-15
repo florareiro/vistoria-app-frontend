4800f81999812dace34d62723e9235aa5395b9c1
florareiro <ceciliaflora@outlook.com.br>
2023-08-15 20:24:26 -0300

    fix cors

--- src/http-common.js
+++ src/http-common.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:
-    "https://api-vistoria-app-florareiro.vercel.app/api" ||
+  baseURL: [
+    "https://api-vistoria-app-florareiro.vercel.app",
     "http://localhost:8080/api",
+  ],
   headers: {
     "Content-type": "application/json",
   },
